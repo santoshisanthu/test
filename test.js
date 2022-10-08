4ea455ade0bc332ceefff3b0bf7eaf2be655e757
santoshi tirumalareddy <114132121+santoshisanthu@users.noreply.github.com>
2022-10-08 12:03:32 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,19 @@
+
+/* Given a number n return the sum of all the consiquite numbers still n
+           sum(n) => 1+2+3+....n                                         */
+
+function consiquiteNumbers(){
+    let n = 0;
+ 
+    for (i = 0; i<=20; i++){
+
+    
+    n += i
+    }
+    
+ console.log(n)
+}
+consiquiteNumbers()
+        
+
+
